6a3115868d78a057fd84c6d8cd03b459270cf199
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-23 21:35:26 +0700

    SET1\Decoder : Refactor class (change arraylist to array). Update encoder and decoder. Add Converter to other decoding format.

--- SET1/Decoder/Hex.java
+++ SET1/Decoder/Hex.java
@@ -1,32 +1,44 @@
 package SET1.Decoder;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 public class Hex {
-  public static ArrayList<Byte> hexDecoder(String s) {
+  // public static ArrayList<Byte> hexDecoder(String s) {
+    // HexMap.setMap();
+    // ArrayList<Byte> temp = new ArrayList<>();
+    // int index = 0;
+    // while (index < s.length()) {
+      // temp.add(HexMap.charToByte.get(s.charAt(index)));
+      // index++;
+    // }
+    // return temp;
+  // }
+
+  // public static String hexEncoder(ArrayList<Byte> buffer){
+    // // Encode binary data from buffer
+    // HexMap.setMap();
+    // String encodedString = "";
+    // for (byte x : buffer) encodedString += HexMap.byteToChar.get(x);
+    // return encodedString;
+  // }
+
+  // Hex Encoder and Decoder
+  public static byte hexDecoder(char character) {
+    // char in hex encoding
+    return HexMap.charToByte.get(character); 
+  }
+
+  public static byte[] hexDecoder(String s) {
+    // s is hex encoded
+    // f is the byte data of the encoded message
     HexMap.setMap();
-    ArrayList<Byte> temp = new ArrayList<>();
-    int index = 0;
-    while (index < s.length()) {
-      temp.add(HexMap.charToByte.get(s.charAt(index)));
-      index++;
+    byte[] f = new byte[s.length()];
+    for (int i = 0; i < s.length(); i++) {
+      f[i] = hexDecoder(s.charAt(i));
     }
-    return temp;
-  }
-
-  public static String hexEncoder(ArrayList<Byte> buffer){
-    HexMap.setMap();
-    String encodedString = "";
-    for (byte x : buffer) encodedString += HexMap.byteToChar.get(x);
-    return encodedString;
-  }
-
-  public static String hexEncoder(byte[] buffer){
-    HexMap.setMap();
-    String encodedString = "";
-    for (byte x : buffer) encodedString += HexMap.byteToChar.get(x);
-    return encodedString;
+    return f;
   }
 
   public static String hexEncoder(byte data) {
+    // Encode one byte of data
     HexMap.setMap();
     byte bitMask = 15; // Have all last 4 bit set
     byte shifter = 4;
@@ -43,5 +55,74 @@ public class Hex {
     data = (byte) (data & bitMask);
     hex += HexMap.byteToChar.get(data);
     return hex;
+  }
+
+  public static String hexEncoder(byte[] buffer){
+    // Encode byte data in hex format
+    HexMap.setMap();
+    String encodedString = "";
+    for (byte x : buffer) encodedString += hexEncoder(x);
+    return encodedString;
+  }
+
+  // Hex converter
+  public static String fromHexToBase64(String hexString) {
+    Base64Map.setMap();
+    String base64String = "";
+    byte key;
+
+    // Set string into array of bytes
+    byte[] temp = hexDecoder(hexString);
+
+    byte bitmask = 3;
+    int idx = 0;
+    int mode = 1;
+    for (int i = 0; i < temp.length - 1; i++) {
+      byte f = temp[idx];
+      byte g = temp[idx + 1];
+
+      if (idx % 2 == mode) {
+        // Take 2 unused bit from before and take 4 bit from the next byte
+        f = (byte) (f & bitmask);
+        f = (byte) (f << 4);
+        // Next byte doesn't have unused bit
+        idx++;
+        // Update the mode
+        mode = (mode + 1) % 2;
+      } else {
+        // Take all 4 bit of current byte and take 2 bit from next byte
+        f = (byte) (f << 2);
+        g = (byte) (g >> 2);
+      }
+      // Convert to base64
+      key = (byte) (f + g);
+      base64String += Base64Map.byteToChar.get(key);
+      idx++;
+    }
+    
+    return base64String;
+  }
+
+  public static byte[] fromHexToAscii(String hex) {
+    byte[] buffer = hexDecoder(hex);
+
+    int length = (int) (buffer.length / 2);
+    byte[] asciiArr = new byte[length];
+
+    // temp variables
+    byte f, g;
+    
+    int pointer1 =  0;
+    int pointer2 = 0;
+
+    while (pointer1 < buffer.length) {
+      f = (byte) (buffer[pointer1] << 4);
+      g = (byte) (f + buffer[pointer1 + 1]);
+      pointer1 += 2;
+      asciiArr[pointer2] = g;
+      pointer2++;
+    }
+
+    return asciiArr;
   }
 }
