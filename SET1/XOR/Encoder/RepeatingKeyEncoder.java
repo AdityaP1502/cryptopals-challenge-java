b9795ab44e563792447428ce43bcac154c596a75
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-23 21:42:13 +0700

    SET1\XOR\Encoder\RepeatingKeyEncoder\RepeatingKeyEncoder.java : Add method to encrypt file

--- SET1/XOR/Encoder/RepeatingKeyEncoder.java
+++ SET1/XOR/Encoder/RepeatingKeyEncoder.java
@@ -1,4 +1,11 @@
 package SET1.XOR.Encoder;
+import java.util.Scanner;
+
+import SET1.Decoder.ASCII;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RepeatingKeyEncoder {
   public static String encrypt(String text, String key) {
@@ -9,4 +16,50 @@ public class RepeatingKeyEncoder {
 
     return encryptedString;
   } 
+
+  public static String encrypt(byte[] buff, byte[] key) {
+    String encryptedString = "";
+    for (int i = 0; i < buff.length; i++) {
+      encryptedString += SingleByteEncoder.encrypt(buff[i], key[(i % key.length)]);
+    }
+
+    return encryptedString;
+  }
+  
+  public static void encrypt(String inPath, String outPath, String key, String encoding) {
+    // Load data from a file
+    // Encrypt
+    // Save to different file
+    File in = new File(inPath);
+    File out = new File(outPath);
+    // temp var
+    String f = "";
+    byte[] g;
+
+    try {
+      Scanner sc = new Scanner(in);
+      while (sc.hasNextLine()) {
+        f += sc.nextLine(); // Normal Text Encoding
+      }
+      sc.close();
+
+      f = encrypt(f , key);
+      g = ASCII.convertTextToBytes(f);
+      FileWriter wr = new FileWriter(out);
+      
+      switch (encoding) {
+        case "Base64":
+          f = ASCII.asciiToBase64(g);
+        case "HEX":
+        default:
+        f = ASCII.asciiToHex(g);
+      }
+
+      wr.write(f);
+      wr.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      System.exit(-1);
+    }
+  }
 }
