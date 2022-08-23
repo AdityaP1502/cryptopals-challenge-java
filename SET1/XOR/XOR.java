1d81fc6ea37a6bc368856665a49255b0892c1196
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-23 21:46:30 +0700

    SET1\XOR\XOR.java : Refactor. Change arraylist to array.

--- SET1/XOR/XOR.java
+++ SET1/XOR/XOR.java
@@ -1,16 +1,16 @@
 package SET1.XOR;
 import SET1.Decoder.Hex;
-import java.util.ArrayList;
+
 public class XOR {
   public static String XORCombination(String hex1, String hex2) {
     // Change to array of bytes
-    ArrayList<Byte> byte1 = Hex.hexDecoder(hex1);
-    ArrayList<Byte> byte2 = Hex.hexDecoder(hex2);
+    byte[] byte1 = Hex.hexDecoder(hex1);
+    byte[] byte2 = Hex.hexDecoder(hex2);
     int index = 0;
     byte temp = 0;
     String xOString = "";
-    while (index < byte1.size()) {
-      temp = (byte) (byte1.get(index) ^ byte2.get(index));
+    while (index < byte1.length) {
+      temp = (byte) (byte1[index] ^ byte2[index]);
       xOString += Hex.hexEncoder(temp);
       index++;
     }
