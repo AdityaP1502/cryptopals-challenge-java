e56b85e020877b86317ad3252170b8fee008da83
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-24 21:20:37 +0700

    SET!/XOR/Decoder/RepeatingKeyDecoder/Pair.java : Add pair class to hold key and its distance value

---
+++ SET1/XOR/Decoder/RepeatingKeyDecoder/Pair.java
@@ -0,0 +1,19 @@
+package SET1.XOR.Decoder.RepeatingKeyDecoder;
+
+public class Pair {
+  final private double a;
+  final private double b;
+
+  Pair(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+  
+  public double getA() {
+    return a;
+  }
+
+  public double getB() {
+    return b;
+  }
+}
