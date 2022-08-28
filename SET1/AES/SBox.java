69d449dbca15a59b140d0a8891f0f12b17dec0cc
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-28 12:53:29 +0700

    SET1/AES/SBox.java : Add Reverse SBox

--- SET1/AES/SBox.java
+++ SET1/AES/SBox.java
@@ -1,27 +1,32 @@
 package SET1.AES;
 
-import SET1.Decoder.Hex;
-
 import java.util.HashMap;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import Encoding.Hex;
+
 public class SBox {
   public static HashMap<Byte, Byte> sbox = new HashMap<>();
   final public static String FILEPATH = "SET1/AES/table.txt";
+  final public static String FILEPATH_REVERSE = "SET1/AES/table_reverse.txt";  
   private static boolean isSet = false;
+  private static int lastState = -1;
 
-  public static void setBox() {
-    if (isSet) return;
+  public static void setBox(int mode) {
+    if (isSet & mode == lastState) return;
     // set flag isSet to true so no need to set multiple times
     isSet = true;
-
+    lastState = mode == 1 ? 1 :  0;
     String temp;
+    File file;
     String[] value;
     byte f;
     
-    File file = new File(FILEPATH);
+    if (mode == 1) file = new File(FILEPATH_REVERSE);
+    else file = new File(FILEPATH);
+    
     byte key = 0;
 
     try (Scanner sc = new Scanner(file)) {
