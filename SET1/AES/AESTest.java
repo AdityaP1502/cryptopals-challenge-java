2f33d76d4382b5249066fff8e9f5ca4d516c6c46
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-28 12:55:40 +0700

    Update Testing Files

--- SET1/AES/AESTest.java
+++ SET1/AES/AESTest.java
@@ -4,49 +4,47 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import SET1.Decoder.ASCII;
-// import SET1.Decoder.ASCII;
-import SET1.Decoder.Hex;
+import Encoding.ASCII;
+import Encoding.Hex;
 
 public class AESTest {
-  public static String displayWordAsHex(byte[][] word) {
-    byte[] f;
-    String wordInHex = "";
-    for (int j = 0; j < 4; j++) {
-      f = word[j];
-      wordInHex += Hex.hexEncoder(f);
-      wordInHex += " ";
-    }
-    return wordInHex.trim();
-  }
+  
   public static void main(String[] args) throws IOException {
-    SBox.setBox();
     // Open file
     String filepath = "SET1/AES/ciphertext.txt";
+    String filepathDecrypt = "SET1/AES/plaintext.txt";
     File file = new File(filepath);
+    File fileDecrypt = new File(filepathDecrypt);
     // Create a new file
     file.createNewFile();
+    fileDecrypt.createNewFile();
 
     FileWriter wr = new FileWriter(file);
     
     // temp var
-    byte[][] f;
     String temp;
 
-    // Key Expansion PASS TESTING
     String key = "YELLOW SUBMARINE";
     System.out.println("Key in hex: " + Hex.hexEncoder(ASCII.convertTextToBytes(key)));
     String message = "I Am Aditya PPPP";
     System.out.println("message in hex: " + Hex.hexEncoder(ASCII.convertTextToBytes(message)));
+
     try {
-      f = AESCipher.encrypt(message, key);
-      temp = displayWordAsHex(f);
+      // encrypt file
+      temp = AESCipher.encrypt(message, key);
       wr.write(temp);
       wr.close();
+
+      // decrypt file
+      wr = new FileWriter(fileDecrypt);
+      byte[] block = Hex.fromHexToAscii(temp.replace(" ", ""));
+      temp = AESCipher.decrypt(block, key);
+      wr.write(temp);
+      wr.close();
+      // decrypt the message
     } catch (InvalidBlockSizeException e) {
       System.out.println(e.getMessage());
       System.out.println(e.getStackTrace());
     }
-    
   }
 }
