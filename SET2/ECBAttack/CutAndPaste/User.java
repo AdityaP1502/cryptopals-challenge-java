127ec0f2a2adafd929ccbc765c76e2ca52221357
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-01 22:08:06 +0700

    User : Add user class for Cut And Paste Attack

---
+++ SET2/ECBAttack/CutAndPaste/User.java
@@ -0,0 +1,77 @@
+package SET2.ECBAttack.CutAndPaste;
+
+import Encoding.ASCII;
+import Encoding.Hex;
+import Encoding.UnrecognizedEncodingException;
+import SET1.AES.AESKey;
+import SET1.AES.ECB.ECB;
+import SET2.EncryptionOracle.EncryptionOracle;
+
+public class User {
+  private String email;
+  private int uid;
+  private String role;
+  private static String key;
+
+  public User(String email) {
+    this.email = email;
+    uid = 10;
+    role = Roles.USER.toString();
+    try {
+      key = AESKey.generateRandomKey("HEX");
+    } catch (UnrecognizedEncodingException e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
+  }
+  
+  public User(String email, int uid, String role) {
+    this.email = email;
+    this.uid = uid;
+    this.role = role;
+  }
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public int getUid() {
+    return uid;
+  }
+
+  public String toString() {
+    String encodedUser = "";
+    encodedUser += "email=" + email + "&";
+    encodedUser += "uid=" + uid + "&";
+    encodedUser += "role=" + role;
+    return encodedUser;
+  }
+
+  public String send() {
+    String encryptedMessage = "";
+    System.out.println(this);
+    encryptedMessage = EncryptionOracle.encryptECB(toString(), "ASCII", key);
+    return encryptedMessage;
+  }
+
+  public static String receive(String encryptedMessage) {
+    String keyInAscii = ASCII.ASCIIDecoder(Hex.fromHexToAscii(key));
+    ECB ecb = new ECB(encryptedMessage, keyInAscii, "HEX");
+    return ecb.decrypt("ASCII");
+  }
+
+  public static User parse(String encodedProfile) {
+    String[] strings = encodedProfile.split("&", -1);
+    String email = strings[0].split("=", 2)[1];
+    int uid = Integer.parseInt(strings[1].split("=", 2)[1]);
+    String role = strings[2].split("=", 2)[1];
+    return new User(email, uid, role);
+  }
+}
