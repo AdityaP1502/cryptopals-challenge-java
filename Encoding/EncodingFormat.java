4a0f24078e4954e4f560464b813b43767d82cb7a
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-01 21:46:56 +0700

    EncodingFormat : Add class to deal with text encoding. Add convert to bytes method given encoding. Add convert to text method given encoding.

---
+++ Encoding/EncodingFormat.java
@@ -0,0 +1,40 @@
+package Encoding;
+
+public class EncodingFormat {
+  public static byte[] convertToBytes(String text, String encoding) throws UnrecognizedEncodingException {
+    byte[] block;
+    switch (encoding) {
+      case "ASCII":
+        block = ASCII.ASCIIEncoder(text);
+        break;
+      case "BASE64":
+        // padding length
+        block = Base64.fromBase64ToAscii(text);
+        break;
+      case "HEX":
+        block = Hex.fromHexToAscii(text);
+        break;
+      default:
+        throw new UnrecognizedEncodingException("Not recoginized encoding format" + encoding);
+    }
+    return block;
+  }
+
+  public static String fromBytesToText(byte[] binaryData, String encoding) throws UnrecognizedEncodingException {
+    String s;
+    switch (encoding) {
+      case "ASCII":
+        s = ASCII.ASCIIDecoder(binaryData);
+        break;
+      case "BASE64":
+        s = Base64.base64Encoder(binaryData);
+        break;
+      case "HEX":
+        s = Hex.hexEncoder(binaryData);
+        break;
+      default:
+        throw new UnrecognizedEncodingException("Not recoginized encoding format" + encoding);
+    }
+    return s;
+  }  
+}
