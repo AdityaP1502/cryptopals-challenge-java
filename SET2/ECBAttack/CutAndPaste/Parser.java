72419f36772838caabb6b795283e2ecd502fae67
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-02 14:53:36 +0700

    Parser : Update parser implementation. Quote metacharacters instead of remove

--- SET2/ECBAttack/CutAndPaste/Parser.java
+++ SET2/ECBAttack/CutAndPaste/Parser.java
@@ -1,19 +1,31 @@
 package SET2.ECBAttack.CutAndPaste;
 
 public class Parser {
-  public static User profileFor(String email) {
+  public static String sanitizeInput(String input) {
     // remove & and =
     int x;
-    while ((x = email.indexOf("&"))!= -1) {
-      // remove & at x
-      email = email.substring(0, x) + email.substring(x + 1, email.length() - 1);
+    String quoteMeta;
+    StringBuilder temp = new StringBuilder(input);
+    while ((x = temp.indexOf("&"))!= -1) {
+      // quote
+      // & -> '&'
+      temp.deleteCharAt(x);
+      quoteMeta = "\'" + "&" + "\'";
+      input = input.substring(0, x) + quoteMeta + input.substring(x + 1, input.length());
     }
 
-    while ((x = email.indexOf("="))!= -1) {
-      // remove & at x
-      email = email.substring(0, x) + email.substring(x + 1, email.length() - 1);
+    while ((x = temp.indexOf("="))!= -1) {
+      temp.deleteCharAt(x);
+      quoteMeta = "\'" + "=" + "\'";
+      input = input.substring(0, x) + quoteMeta + input.substring(x + 1, input.length());
     }
 
-    return new User(email);
+    while ((x = temp.indexOf(";"))!= -1) {
+      temp.deleteCharAt(x);
+      quoteMeta = "\'" + ";" + "\'";
+      input = input.substring(0, x) + quoteMeta + input.substring(x + 1, input.length());
+    }
+
+    return input;
   }
 }
