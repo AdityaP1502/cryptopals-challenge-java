929ef49224239c3ec22a61c6d19a44997e5d882a
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-02 14:57:28 +0700

    Update testing files

---
+++ SET1/AES/CBC/CBCTest2.java
@@ -0,0 +1,48 @@
+package SET1.AES.CBC;
+
+import Encoding.ASCII;
+import Encoding.EncodingFormat;
+import Encoding.Hex;
+import Encoding.UnrecognizedEncodingException;
+import SET1.AES.AESCipher;
+import SET1.AES.InvalidBlockSizeException;
+import SET1.AES.Words;
+
+public class CBCTest2 {
+  public static void main(String[] args) throws UnrecognizedEncodingException, InvalidBlockSizeException {
+    String IV = "1f265dfcef538e67a3c71230ec4c72cb";
+    String key = "bae9e8ddf212132396060a8ff1a4da1e";
+    String message = "comment1=cooking";
+
+    // convert to bytes
+    byte[] ivBytes = EncodingFormat.convertToBytes(IV, "HEX");
+    String keyInASCII = ASCII.ASCIIDecoder(Hex.fromHexToAscii(key));
+    byte[] messageByte = EncodingFormat.convertToBytes(message, "ASCII");
+    
+
+    // XOR with iv
+    Words.XOR(messageByte, ivBytes);
+    System.out.println(Hex.hexEncoder(messageByte));
+    String decryptedMessage = AESCipher.encrypt(messageByte, keyInASCII).replace(" ", "");
+    System.out.println(decryptedMessage);
+
+    // decrypt
+    byte[] decryptedBlock = EncodingFormat.convertToBytes(decryptedMessage, "HEX");
+    String beforeIV = AESCipher.decrypt(decryptedBlock, keyInASCII).replace(" ", "");
+    System.out.println(beforeIV);
+    String originalMessage;
+    byte[] beforeIVBytes = EncodingFormat.convertToBytes(beforeIV, "HEX");
+    Words.XOR(beforeIVBytes, ivBytes);
+    originalMessage = ASCII.ASCIIDecoder(beforeIVBytes);
+    System.out.println(originalMessage);
+
+    CBC cbc = new CBC(message, keyInASCII, IV, "ASCII");
+    decryptedMessage = cbc.encrypt();
+    System.out.println(decryptedMessage);
+    cbc = new CBC(decryptedMessage, keyInASCII, IV, "HEX");
+    originalMessage = cbc.decrypt();
+    originalMessage = ASCII.ASCIIDecoder(Hex.fromHexToAscii(originalMessage));
+    System.out.println(originalMessage);
+  }
+  
+}
---
+++ SET2/CBCAttack/BitFlipping/AttackTest.java
@@ -0,0 +1,8 @@
+package SET2.CBCAttack.BitFlipping;
+
+public class AttackTest {
+  public static void main(String[] args) {
+    BitFlipAttack atck = new BitFlipAttack();
+    System.out.println(atck.attack());
+  }
+}
