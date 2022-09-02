c6fd8916ea1812812be219d2b9867dd721ab4eb0
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-02 09:42:10 +0700

    PKCS : Add getPadLength and removePadding method.

--- SET1/AES/Padding/PKCS.java
+++ SET1/AES/Padding/PKCS.java
@@ -1,5 +1,8 @@
 package SET1.AES.Padding;
 
+import Encoding.EncodingFormat;
+import Encoding.UnrecognizedEncodingException;
+
 public class PKCS {
   public static void pad(byte[] block, int padLength) {
     // Assume block already reserved 
@@ -10,5 +13,35 @@ public class PKCS {
     for (int i = 0; i < padLength; i++) {
       block[lastIdx - i] = padByte;
     }
+  }
+
+  public static int getPadLength(String text, String encoding) throws InvalidPaddingException {
+    // text with padding
+    int padLength = -1;
+    try {
+      byte[] bytes = EncodingFormat.convertToBytes(text, encoding);
+      int lastIdx = bytes.length - 1;
+      byte lastChar = bytes[lastIdx];
+      padLength = lastChar;
+      for (int i = lastIdx; i > lastIdx - padLength; i--) {
+        if (bytes[i] != lastChar) throw new InvalidPaddingException("Invalid PKCS Padding");
+      }
+    } catch (UnrecognizedEncodingException e) {
+      e.printStackTrace();
+    }
+
+    return padLength;
+  }
+
+  public static String removePadding(String text, String encoding) throws InvalidPaddingException {
+    StringBuilder x = new StringBuilder(text);
+    int padLength = getPadLength(text, encoding);
+
+    for (int i = 0; i < padLength; i++) {
+      // remove padding
+      x.deleteCharAt(x.length() - 1);
+    }
+
+    return x.toString();
   }
 }
