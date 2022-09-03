a048c6607c126b33218d85a1391bace72d170f01
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-04 01:03:22 +0700

    PKCS : Change stringuilder into byte[] array in removePadding

--- AES/Padding/PKCS.java
+++ AES/Padding/PKCS.java
@@ -1,5 +1,6 @@
 package AES.Padding;
 
+import Encoding.ASCII;
 import Encoding.EncodingFormat;
 import Encoding.UnrecognizedEncodingException;
 
@@ -15,33 +16,40 @@ public class PKCS {
     }
   }
 
-  public static int getPadLength(String text, String encoding) throws InvalidPaddingException {
+  public static int getPadLength(byte[] bytes) throws InvalidPaddingException {
     // text with padding
     int padLength = -1;
-    try {
-      byte[] bytes = EncodingFormat.convertToBytes(text, encoding);
-      int lastIdx = bytes.length - 1;
-      byte lastChar = bytes[lastIdx];
-      padLength = lastChar;
-      for (int i = lastIdx; i > lastIdx - padLength; i--) {
-        if (bytes[i] != lastChar) throw new InvalidPaddingException("Invalid PKCS Padding");
-      }
-    } catch (UnrecognizedEncodingException e) {
-      e.printStackTrace();
+    int lastIdx = bytes.length - 1;
+    byte lastChar = bytes[lastIdx];
+
+    padLength = lastChar;
+    if (padLength <= 0) throw new InvalidPaddingException("Invalid PKCS Padding");
+    for (int i = lastIdx; i > lastIdx - padLength; i--) {
+      if (bytes[i] != lastChar || (bytes[i] > 16 || bytes[i] < 0)) throw new InvalidPaddingException("Invalid PKCS Padding");
     }
 
     return padLength;
   }
 
   public static String removePadding(String text, String encoding) throws InvalidPaddingException {
-    StringBuilder x = new StringBuilder(text);
-    int padLength = getPadLength(text, encoding);
+    // change text into ascii
+    byte[] bytes;
+    StringBuilder x = null;
+    try {
+      bytes = EncodingFormat.convertToBytes(text, encoding);
+      int padLength = getPadLength(bytes);
+      x = new StringBuilder(ASCII.ASCIIDecoder(bytes));
 
-    for (int i = 0; i < padLength; i++) {
-      // remove padding
-      x.deleteCharAt(x.length() - 1);
+      for (int i = 0; i < padLength; i++) {
+        // remove padding
+        x.deleteCharAt(x.length() - 1);
+      }
+
+    } catch (UnrecognizedEncodingException e) {
+      e.printStackTrace();
+      System.exit(-1);
     }
-
+    
     return x.toString();
   }
 }
