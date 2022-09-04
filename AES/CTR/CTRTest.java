57079f965339ee00e6ee13afaed2469f826dbb5b
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-04 11:35:48 +0700

    Update testing files

---
+++ AES/CTR/CTRTest.java
@@ -0,0 +1,32 @@
+package AES.CTR;
+
+import AES.InvalidBlockSizeException;
+import Encoding.ASCII;
+import Encoding.Hex;
+import Encoding.UnrecognizedEncodingException;
+
+public class CTRTest {
+  public static void main(String[] args) {
+    String nonce = "0000000000000000";
+    String KEY = "YELLOW SUBMARINE";
+    String text = "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ";
+    String message = "Hello, my name is I Made Aditya Putra Jaya Diwangsa";
+    try {
+      CTR ctr = new CTR(text, "BASE64", KEY, nonce);
+      String originalMessage = ctr.decrypt();
+      originalMessage = ASCII.ASCIIDecoder(Hex.fromHexToAscii(originalMessage));
+      System.out.println(originalMessage);
+
+      ctr.setMessage(message, "ASCII");
+      String gibberish = ctr.encrypt();
+      ctr.setMessage(gibberish, "HEX");
+      originalMessage = ctr.decrypt();
+      originalMessage = ASCII.ASCIIDecoder(Hex.fromHexToAscii(originalMessage));
+      System.out.println(originalMessage);
+    
+    } catch (UnrecognizedEncodingException | InvalidBlockSizeException e) {
+      System.exit(-1);
+      e.printStackTrace();
+    }
+  }
+}
--- SET1/XOR/XORTest.java
+++ SET1/XOR/XORTest.java
@@ -2,13 +2,13 @@ package SET1.XOR;
 
 public class XORTest {
   public static void main(String[] args) {
-    String s1 = "1c0111001f010100061a024b53535009181c";
-    String s2 = "686974207468652062756c6c277320657965";
-    String expected = "746865206b696420646f6e277420706c6179";
+    // String s1 = "1c0111001f010100061a024b53535009181c";
+    // String s2 = "686974207468652062756c6c277320657965";
+    // String expected = "746865206b696420646f6e277420706c6179";
 
-    String stringResult = XOR.XORCombination(s1, s2);
-    System.out.println(stringResult);
-    System.out.println(stringResult.equals(expected));
+    // String stringResult = XOR.XORCombination(s1, s2, -);
+    // System.out.println(stringResult);
+    // System.out.println(stringResult.equals(expected));
   }
 
 }
