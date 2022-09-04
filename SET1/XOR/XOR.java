20848d2775fdd87f813743b76012304e0635cd1f
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-04 11:35:26 +0700

    XOR : Change XORCombination method to work with CTR

--- SET1/XOR/XOR.java
+++ SET1/XOR/XOR.java
@@ -2,19 +2,17 @@ package SET1.XOR;
 import Encoding.Hex;
 
 public class XOR {
-  public static String XORCombination(String hex1, String hex2) {
+  public static String XORCombination(byte[] bytes1, byte[] bytes2, int start) {
     // Change to array of bytes
-    byte[] byte1 = Hex.hexDecoder(hex1);
-    byte[] byte2 = Hex.hexDecoder(hex2);
-    int index = 0;
+    int n = bytes1.length - start;
     byte temp = 0;
-    String xOString = "";
-    while (index < byte1.length) {
-      temp = (byte) (byte1[index] ^ byte2[index]);
-      xOString += Hex.hexEncoder(temp);
-      index++;
+    String XORString = "";
+    
+    for (int i = 0; i < Math.min(n, bytes2.length); i++) {
+      temp = (byte) (bytes1[start + i] ^ bytes2[i]);
+      XORString += Hex.hexEncoder(temp);
     }
     
-    return xOString;
+    return XORString;
   }
 }
