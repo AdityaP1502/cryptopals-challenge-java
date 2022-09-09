8ff6ceacab608cdb6d787b56e6583a225c51c256
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-09 12:04:25 +0700

    CrackTheSeed : Add method to crack seed from known output

--- PRNG/MersenneTwister/CrackTheSeed.java
+++ PRNG/MersenneTwister/CrackTheSeed.java
@@ -13,7 +13,7 @@ public class CrackTheSeed {
     return mt.nextInt();
   }
 
-  public static int findSeed() throws InterruptedException {
+  public static int findSeedTime() throws InterruptedException {
     long initialTime = System.currentTimeMillis();
     int seed = -1;
     MT19937 mt = new MT19937();
@@ -30,6 +30,30 @@ public class CrackTheSeed {
     }
 
     return seed;
+  }
 
+  public static int crackTheSeed(long number, int statePos) {
+    // given some state at some pos 
+    // brute force the seed that give the same value
+    // seed is a 16 bit numbers
+    int seedMaxRange = (1 << 16) - 1;
+    MT19937 generator;
+    long output = 0;
+    for (int i = 0; i <= seedMaxRange; i ++) {
+      generator = new MT19937(i);
+      // tap the generator for statePos amount
+      for (int j = 0; j < (statePos + 1); j++) {
+        output = generator.nextInt();
+      }
+
+      // output would be state at statePos
+      if (output == number) {
+        // found the state
+        return i;
+      }
+    }
+
+    // seed isn't 16 bit 
+    return -1;
   }
 }
