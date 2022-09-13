d213f38cfa6c7d13c0efd79b62ec2a7c9f2fb7fe
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-14 00:04:29 +0700

    EditAttack: Add attack ror CTR edit method

---
+++ SET4/EditAttack.java
@@ -0,0 +1,64 @@
+package SET4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import AES.InvalidBlockSizeException;
+import AES.CTR.CTR;
+import Encoding.ASCII;
+import Encoding.Hex;
+import Encoding.UnrecognizedEncodingException;
+import SET1.XOR.XOR;
+
+public class EditAttack {
+  public static String readFiles() throws FileNotFoundException {
+    File file = new File("SET4/file.txt");
+    Scanner sc = new Scanner(file);
+    String message = "";
+
+    while (sc.hasNextLine()) {
+      message += sc.nextLine();
+    }
+
+    sc.close();
+    return message;
+  }
+  public static void main(String[] args) {
+    String encryptedMessage = null;
+    CTR ctr = null;
+
+    try {
+      ctr = new CTR();
+      String message = readFiles();
+      ctr.setMessage(message, "ASCII");
+      encryptedMessage = ctr.decrypt();
+      // edit the ciphertext
+      int length = encryptedMessage.length();
+      // craft string with byte value 0
+      String attackString = "";
+      for (int i = 0; i < length; i++) {
+        attackString += "0";
+      }
+
+      // edit the ciphertext, overwrite all of the string
+      // P XOR 0 = P
+      // use edit to get the keystream
+      String keystream = ctr.edit(0, attackString);
+      // from keystream get original message
+      byte[] x = Hex.fromHexToAscii(encryptedMessage);
+      byte[] y = Hex.fromHexToAscii(keystream);
+      String decrypedMessage = XOR.XORCombination(x, y, 0);
+      decrypedMessage = ASCII.ASCIIDecoder(Hex.fromHexToAscii(decrypedMessage));
+      System.out.println(decrypedMessage);
+
+    } catch (UnrecognizedEncodingException | FileNotFoundException | InvalidBlockSizeException e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
+
+    
+
+
+  }
+}
