bf2395d08ed4352f587e286f89090ca777c3d801
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-19 00:20:56 +0700

    LengthExtensionAttack: Add length Extension attack to MD4

--- SET4/LengthExtensionAttack.java
+++ SET4/LengthExtensionAttack.java
@@ -3,14 +3,16 @@ package SET4;
 import Encoding.Hex;
 import Encoding.UnrecognizedEncodingException;
 import Hash.MAC;
+import Hash.MD4;
 import Hash.SHA1;
 
 public class LengthExtensionAttack {
-  public static String padding(String message, int keySize) {
+  public static String padding(String message, int keySize, String type) {
     int messageLength = (message.length() + keySize) * 8; // message and key in bytes
     String padding = messageLength % 8 == 0 ? "80" : "";
 
     int n = messageLength + padding.length() * 4;
+    n = n % 512;
     n = n <= 448 ? (448 - n) : 448 + (512 - n);
     n /= 4;
 
@@ -18,18 +20,29 @@ public class LengthExtensionAttack {
       padding += '0';
     }
 
-    padding += Hex.hexEncoder((long) messageLength);
+    padding += type.equals("SHA1") ? Hex.hexEncoder((long) messageLength) : MD4.convertToLittleEndian(Hex.hexEncoder((long) messageLength));
     return padding;
   }
   public static void main(String[] args) throws UnrecognizedEncodingException {
+    // SHA1 Length Extension Attack
     String message = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon";
-    String hash = MAC.sign(message);
-    // key is 16 bytes)
+    String hash = MAC.sign(message, "SHA1");
+    // key is 16 bytes
     String newMessage = ";admin=true";
-    message = message + Hex.fromHexToAscii(padding(message, 16)) + newMessage;
+    message = message + Hex.fromHexToAscii(padding(message, 16, "SHA1")) + newMessage;
     // attack
-    String sign = MAC.sign(message);
+    String sign = MAC.sign(message, "SHA1");
     hash = SHA1.forge(newMessage, hash, (message.length() + 16) * 8);
+    System.out.println("Attack success: " + sign.equals(hash));
+
+    System.out.println("This is MD4 Attack");
+    // MD4 Length Extension Attack
+    message = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon";
+    hash = MAC.sign(message, "MD4");
+    message = message + Hex.fromHexToAscii(padding(message, 16, "MD4")) + newMessage;
+    // attack
+    sign = MAC.sign(message, "MD4");
+    hash = MD4.forge(newMessage, hash, (message.length() + 16) * 8);
     System.out.println("Attack success: " + sign.equals(hash));
   }
 }
