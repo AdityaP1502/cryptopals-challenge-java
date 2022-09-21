285b48ba7e1c83643c0e5a5fbd68c081abea5482
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-21 09:31:43 +0700

    fileHandler: Add read function to read file async

--- SET4/Challenge31/Backend/fileHandler.js
+++ SET4/Challenge31/Backend/fileHandler.js
@@ -1,10 +1,22 @@
-const {path} = require('path');
-
-const readFile = (filename) => {
+const fs = require('fs').promises;
+const path = require('path');
+const read = async (filename) => {
   // file is assumed to be in the same directory
-  // TODO: Read file
+  let err = 0;
+  let data = null;
+  const filepath = path.resolve("SET4", "Challenge31", "Backend", filename);
+  try {
+    data = await fs.readFile(filepath, encoding = 'utf-8');
+  } catch (e) {
+    console.log(e.name);
+    console.log(e.message);
+    console.log(e.stack);
+    err = 1; 
+  } finally {
+    return {data, err};
+  }
 }
 
 module.exports = {
-  readFile,
+  read,
 }
