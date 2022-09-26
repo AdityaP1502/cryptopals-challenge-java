fef55b965cf46fd44925e93effb3ad522c1504eb
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-27 00:43:08 +0700

    Pair: Add pair class

---
+++ SET4/Challenge31/Pair.java
@@ -0,0 +1,11 @@
+package SET4.Challenge31;
+
+public class Pair {
+  public int bytes;
+  public long timing;
+  
+  public Pair(int bytes, long timing) {
+    this.bytes = bytes;
+    this.timing = timing;
+  } 
+}
