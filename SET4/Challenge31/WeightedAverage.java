ca3fe509f0a1d1da9c9313c942b076b380236318
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-27 12:06:06 +0700

    WeightedAverage: Update average calculation. add term to punish high timing value caused by a freeze or other source.

--- SET4/Challenge31/WeightedAverage.java
+++ SET4/Challenge31/WeightedAverage.java
@@ -4,17 +4,42 @@ public class WeightedAverage {
   // Average to clean noise in timing
   private static int range = 10;
   private static int[] bin;
-  private static long[] sum_bin;
+  private static long[] avg_bin;
   private static double[] weights;
 
-  private static void setWeights(long min, long max, int n) {
-    long range = (long) Math.pow((max - min), 2) + 1;
+  private static long[] averageBinValue(int maxBin) {
+    long maxVal = 0, minVal = -1, maxBinVal = 0;
+
+    for (int i = 0; i < avg_bin.length; i++) {
+      if (bin[i] == 0) continue;
+      avg_bin[i] = avg_bin[i] / bin[i];
+      maxVal = Math.max(avg_bin[i], maxVal);
+      minVal = minVal < 0 ? avg_bin[i] : Math.min(avg_bin[i], minVal);
+      if (bin[i] == maxBin) maxBinVal = avg_bin[i];
+    }
+
+    maxVal = Math.abs(maxVal - maxBinVal);
+    minVal = Math.abs(minVal - maxBinVal);
+    long[] val = {Math.max(maxVal, minVal), maxBinVal};
+    return val;
+  }
+
+  private static long calculateDistanceSquared(long a, long b) {
+    return (long) Math.pow(b - a, 2);
+  }
+
+  private static long calculateDistanceRoot(long a, long b) {
+    return (long) Math.sqrt(Math.abs(a - b));
+  }
+
+  private static void setWeights(long min, long max, long maxDistance, long maxBinVal, int n) {
+    long range = calculateDistanceSquared(max, min) + calculateDistanceRoot(maxBinVal, maxDistance) + 1;
     weights = new double[n];
     long sum = 0;
     // set weights arr
     for (int i = 0; i < n; i++) {
       if (bin[i] == 0) continue;
-      weights[i] = range - (long) Math.pow((max - bin[i]), 2);
+      weights[i] = range - (calculateDistanceSquared(max, bin[i]) + calculateDistanceRoot(maxBinVal, avg_bin[i]));
       sum += weights[i];
     }
     // normalize weights
@@ -24,8 +49,8 @@ public class WeightedAverage {
     
   }
 
-  private static long findMax() {
-    long max = 0;
+  private static int findMax() {
+    int max = 0;
     for (int i = 0; i < bin.length; i++) {
       max = Math.max(bin[i], max);
     }
@@ -33,24 +58,24 @@ public class WeightedAverage {
     return max;
   }
 
-  private static long findMin() {
-    long min = bin[0];
+  private static int findMin() {
+    int min = -1;
     for (int i = 0; i < bin.length; i++) {
-      if (bin[i] > 0 && min > bin[i]) {
-        min = bin[i];
-      }
+      if (bin[i] > 0) min = min < 0 ? bin[i] : Math.min(bin[i], min);
     }
     return min;
   }
 
   public static long cleanNoise(long[] timings, long maxTimings) {
     // remove noise from timing
+    long[] f;
     int n = (int) Math.ceilDiv(maxTimings, range) + 1;
   
     bin = new int[n];
-    sum_bin = new long[n];
+    avg_bin = new long[n];
 
-    long max, min;
+    int max, min;
+    long maxDistance, maxBinVal;
     long average = 0;
     int idx;
 
@@ -58,20 +83,25 @@ public class WeightedAverage {
     for (long timing : timings) {
       idx = (int) Math.floorDiv(timing, range);
       bin[idx] += 1;
-      sum_bin[idx] += timing;
+      avg_bin[idx] += timing;
     }
     
-    // find max and min
+    // find max and min bin
     max = findMax();
     min = findMin();
 
+    // find bin average value and its extreme
+    f = averageBinValue(max);
+    maxDistance = f[0];
+    maxBinVal = f[1];
+
     // set weights
-    setWeights(min, max, n);
+    setWeights(min, max, maxDistance, maxBinVal, n);
 
     // calculate average
     for (int i = 0; i < n; i++) {
       if (bin[i] == 0) continue;
-      average += (sum_bin[i] / bin[i]) * weights[i];
+      average += (avg_bin[i]) * weights[i];
     }
     return average;
   }
