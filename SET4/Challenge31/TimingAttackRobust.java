e3e3aea5c387e519b8ae322af3b323e2fdcbc3ed
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-27 12:08:18 +0700

    TimingAttackRobust: Update attack routine

--- SET4/Challenge31/TimingAttackRobust.java
+++ SET4/Challenge31/TimingAttackRobust.java
@@ -8,9 +8,9 @@ import java.util.Comparator;
 
 
 public class TimingAttackRobust {
+  public static String signature = "";
   public static String file = "passwd.txt";
 	public static final int MAX_COUNT = 10;
-  public static final int MAX_RETRY = 5;
   public static long[] maxTimes = new long[20];
   public static long[] avgTimes = new long[20];
   public static int PICK = 10;
@@ -31,67 +31,23 @@ public class TimingAttackRobust {
     return response;
   }
 
-  public static boolean check(String signature, long lastAverageTime) throws IOException {
+  public static Timing bruteForce(String signature, boolean verbose) throws IOException {
+    Pair temp;
     String g;
+    long cleanTiming, averageTime, startTime, endTime, f;
 
-    long maxTiming = 0;
-    long sumTime = 0;
-
-    long startTime, endTime, f;
-    long cleanTiming, averageTime;
-		String padding = "0".repeat(40 - (signature.length() + 2));
+    long maxTiming = 0, sumTime = 0;
     byte x = 0;
-
-    long[] timings = new long[MAX_RETRY];
-    for (int i = 0; i < 256; i++) {
-      for (int j = 0; j < MAX_RETRY; j++) {
-        g = signature + Hex.hexEncoder(x);
-        g += padding; // Padding with gibbersih
-        // System.out.println("Sent signature: " + g);
-        // request time
-        startTime = System.currentTimeMillis();
-        sendRequest(g);
-        endTime = System.currentTimeMillis();
-        // get process time
-        f = endTime - startTime;
-        timings[j] = f;
-        maxTiming = Math.max(f, maxTiming);
-      }
-      // remove noise from timing
-			cleanTiming = WeightedAverage.cleanNoise(timings, maxTiming);
-      sumTime += cleanTiming;
-      x++;
-    }
-		
-		System.out.println(lastAverageTime);
-    averageTime = sumTime / 256;
-    System.out.println(averageTime);
-    return averageTime > lastAverageTime + WINDOW;
-  }
-
-  public static String attack(String signature) throws IOException {
-    // get potential bytes that has the highest timing
-    String g;
-
-    long maxTiming = 0;
-    long sumTime = 0;
-
-    long startTime, endTime, f;
-    long cleanTiming, averageTime;
+    int padLength = Math.max(0, 18 - signature.length());
+    String padding = "0".repeat(padLength);
 
     long[] timings = new long[MAX_COUNT];
     Pair[] cleanTimings = new Pair[256];
-
-    Pair temp;
-    byte x = 0;
-    String padding = "0".repeat(40 - (signature.length() + 2));
-
     sendRequest("0000000000000000000000000000000000000000");
-    
     for (int i = 0; i < 256; i++) {
       g = signature + Hex.hexEncoder(x);
       g += padding;
-      System.out.println("Sent signature: " + g);
+      if (verbose) System.out.println("Sent signature: " + g);
       for (int j = 0; j < MAX_COUNT; j++) {
         // request time
         startTime = System.currentTimeMillis();
@@ -112,51 +68,62 @@ public class TimingAttackRobust {
     }
 
     averageTime = sumTime / 256;
-    return getBytes(signature, cleanTimings, averageTime);
+    return new Timing(cleanTimings, averageTime);
   }
 
-  public static String getBytes(String signature, Pair[] timings, long averageTime) throws IOException {
+  public static Timing getBytes(Pair[] timings, long averageTime) throws IOException, ByteNotFoundException {
     String g, f;
+    Timing res;
     // from potential bytes check if 
     Comparator<Pair> c = Collections.reverseOrder(Comparator.comparingLong((o) -> o.timing));
     // sort in reverse order
     Arrays.sort(timings, c);
+    System.out.println("Top " + PICK + " bytes with the biggest timing");
     for (int i = 0; i < PICK; i++) {
       System.out.println("Bytes: " + timings[i].bytes + " ,timing: " + timings[i].timing);
     }
     
     // check the first N bytes
+    System.out.println("Trying each bytes...");
     for (int i = 0; i < PICK; i++) {
       f = Hex.hexEncoder((byte) timings[i].bytes);
       g = signature + f;
-      if (check(g, averageTime)) {
-        return f;
+      res = bruteForce(g, false);
+      if (res.averageTime > averageTime + WINDOW) {
+        System.out.println("Found correct bytes: " + f);
+        // update signature
+        signature += f;
+        return res;
       }
+      System.out.println("Last Average Time: " + averageTime + ", current Average Time: " + res.averageTime);
+      System.out.println("Byte " + f +  ": false");
     }
 
-    return "";
+    throw new ByteNotFoundException("Failed To Find a the correct bytes. Rebuild from " + signature);
   }
-  public static void main(String[] args) throws IOException {
-		System.out.println("ATTACK MODE");
-		String signature = "";
+
+  public static void attack() throws IOException, ByteNotFoundException {
+    System.out.println("INIT Timing with signature: " + signature);
+    Timing f = bruteForce(signature, false); // init timing value
+    System.out.println("Init Done!");
+    System.out.println("Bruteforce signature initiated...");
+    for (int i = (signature.length() / 2) + 1; i <= 20; i++ ) {
+      f = getBytes(f.timing, f.averageTime);
+      System.out.println("Finding next bytes...");
+    }
+  }
+
+  
+  public static void main(String[] args) throws IOException, ByteNotFoundException {
+		System.out.println("ATTACK");
+		String f;
 	
 		if (args.length == 1) {
 			signature = args[0];
 			System.out.println("Rebuilding from: " + signature);
 		}
     
-    String f;
-    for (int i = (signature.length() / 2) + 1; i <= 20; i++) {
-			System.out.println("BytesID: " + i);
-			f = attack(signature);
-      if (f.equals("")) {
-        System.out.println("Failed!");
-        System.exit(-1);
-      }
-
-			System.out.println("Correct bytes: " + f);
-			signature += f;
-		}
+    attack();
 
 		System.out.println("Signature found: " + signature);
 		System.out.println("Sending request with valid signature!");
