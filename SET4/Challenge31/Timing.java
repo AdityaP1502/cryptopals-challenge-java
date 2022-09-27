d45ab6305f8f39545624b352bb41e8c6ac68922b
AdityaP1502 <i.m.aditya015@gmail.com>
2022-09-27 12:08:00 +0700

    Timing: Class for return value in TimingAttackRobust

---
+++ SET4/Challenge31/Timing.java
@@ -0,0 +1,11 @@
+package SET4.Challenge31;
+
+public class Timing {
+  public Pair[] timing;
+  public long averageTime;
+  
+  public Timing(Pair[] timing, long averageTime) {
+    this.timing = timing;
+    this.averageTime = averageTime;
+  }
+}
